bf251aff5263a5fcb9e6522594a3fbcea91a9d55
Pedro Nakano Tramontin <pnakano@uolinc.com>
2018-06-25 18:05:00 -0300

    Google Kubernetes Engine

--- sync-client/src/main/java/br/com/pedront/sync/client/feign/SyncServer.java
+++ sync-client/src/main/java/br/com/pedront/sync/client/feign/SyncServer.java
@@ -1,10 +1,11 @@
 package br.com.pedront.sync.client.feign;
 
-import br.com.pedront.sync.server.controller.dto.NoteVO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "sync-server-rest-api", url = "http://localhost:8080/")
+import br.com.pedront.sync.server.controller.dto.NoteVO;
+
+@FeignClient(name = "sync-server-rest-api", url = "http://35.192.146.150:8080/")
 public interface SyncServer {
 
     @PostMapping("/notes")
